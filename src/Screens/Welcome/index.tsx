744f05c1bf7b9f12d9596da2df63de3fe14ee2ac
Sameer <sameer.sikandar@metafroliclabs.com>
2024-09-09 07:18:53 -0700

    Splash Screen

---
+++ src/Screens/Welcome/index.tsx
@@ -0,0 +1,13 @@
+import {Text, View} from 'react-native';
+import { styles } from './style';
+
+
+function WelcomeScreen() {
+  return (
+    <View style = {styles}>
+      <Text>Welcome</Text>
+    </View>
+  );
+}
+
+export default WelcomeScreen;
