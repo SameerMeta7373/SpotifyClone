cb102921496674753ffb13112a92801b834b9b54
Sameer <sameer.sikandar@metafroliclabs.com>
2024-09-12 07:14:22 -0700

    Music Player complete Screen

--- src/Screens/MusicPlayer/index.tsx
+++ src/Screens/MusicPlayer/index.tsx
@@ -1,9 +1,76 @@
-import {Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import {image} from '../../Components/image';
+import Icons from '../../Components/ImageIcons/Icons';
+import {ScrollView} from 'react-native-gesture-handler';
+import Slider from '@react-native-community/slider';
+import colors from '../../Utils/colors';
+import {useState} from 'react';
+import {styles} from './style';
 
 export function MusicPlayer() {
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+
+  function Song() {
+    setIsPlaying(prevIsPlaying => !prevIsPlaying);
+  }
+  function randomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 8)];
+    }
+    return color;
+  }
+
   return (
-    <View>
-      <Text> Hello</Text>
-    </View>
+    <ScrollView>
+      <LinearGradient
+        colors={['#5E5A4F', '#35322D', '#1D1C1A']}
+        style={{flex :1}}>
+        <View style={{alignSelf: 'center', paddingVertical: 8}}>
+          <Text style={styles.rootHeader}>PLAYING FROM SEARCH</Text>
+          <Text style={styles.headerSongName}>“stay” in Songs</Text>
+        </View>
+        <View style={styles.ImageContainer}>
+          <Image style={styles.ImageStyle} source={image.StayPoster} />
+        </View>
+        <View style={styles.songNameContainer}>
+          <View style={{width: 300, marginHorizontal: 20}}>
+            <Text style={styles.songName}>STAY (with Justin Bieber)</Text>
+            <Text style={styles.singerName}>The Kid LAROI, Justin Bieber</Text>
+          </View>
+          <Icons source={image.Heart} />
+        </View>
+        <Slider
+          style={styles.slider}
+          minimumValue={0}
+          maximumValue={1}
+          thumbTintColor={colors.primaryWhite}
+          minimumTrackTintColor={colors.primaryWhite}
+          maximumTrackTintColor=" #3D3C38"
+        />
+        <View style={styles.songControllContainer}>
+          <Icons source={image.Shuffle} />
+          <Icons style={{height: 30, width: 25}} source={image.PreviousPlay} />
+
+          <Icons
+            style={{height: 70, width: 70}}
+            source={isPlaying ? image.Pause : image.WhitePlayButton}
+            onPress={Song}
+          />
+          <Icons style={{height: 30, width: 25}} source={image.NextPlay} />
+          <Icons source={image.Loop} />
+        </View>
+
+        <View style={styles.optionsContainer}>
+          <Icons style={{height: 30, width: 30}} source={image.Cast} />
+          <Icons style={{height: 20, width: 20}} source={image.Share} />
+        </View>
+        <View style={[styles.lyrics, {backgroundColor: randomColor()}]}>
+          <Text style={styles.lyricsHeading}>Lyrics</Text>
+        </View>
+      </LinearGradient>
+    </ScrollView>
   );
 }
---
+++ src/Screens/MusicPlayer/style.ts
@@ -0,0 +1,67 @@
+import {StyleSheet} from 'react-native';
+import colors from '../../Utils/colors';
+
+export const styles = StyleSheet.create({
+  ImageContainer: {
+    paddingHorizontal: 25,
+    paddingVertical: 40,
+  },
+  ImageStyle: {
+    height: 410,
+    width: 362,
+  },
+  songNameContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  songName: {
+    fontSize: 24,
+    color: colors.primaryWhite,
+    fontWeight: '600',
+  },
+  singerName: {
+    fontSize: 20,
+    color: colors.primaryWhite,
+    fontWeight: '600',
+  },
+  slider: {
+    width: 350,
+    height: 60,
+    alignSelf: 'center',
+  },
+  songControllContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+  optionsContainer: {
+    flexDirection: 'row',
+    paddingVertical: 20,
+    marginHorizontal: '10%',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  lyrics: {
+    minHeight: 600,
+    margin: 30,
+    borderRadius: 16,
+  },
+  lyricsHeading: {
+    fontSize: 20,
+    color: colors.primaryWhite,
+    fontWeight: '700',
+    padding: 20,
+  },
+  headerSongName: {
+    fontSize: 19,
+    color: colors.primaryWhite,
+    fontWeight: '600',
+    alignSelf: 'center',
+  },
+  rootHeader: {
+    fontSize: 16,
+    color: colors.primaryWhite,
+    fontWeight: '500',
+  },
+});
